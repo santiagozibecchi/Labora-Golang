5464d905c3f32a3a9e04705f96d02a132d582e9a
Santiago Zibecchi <santiago.zibe@gmail.com>
2023-12-19 11:50:16 -0300

    Ejercicio 2 errores

--- Errores/ejercicio2/main.go
+++ Errores/ejercicio2/main.go
@@ -1,5 +1,34 @@
 package main
 
+import "fmt"
+
+/*
+## **Ejercicio 2: Validación de entrada**
+Crea una función que acepte un string y devuelva un error si el string está vacío o si es demasiado largo (por ejemplo, más de 100 caracteres).
+*/
+
 func main()  {
 	
+
+	value, err := validateStringInput("DuiahpiuhfuipasdfiupsahfpuiosaDuiahpiuhfuipasdfiupsahfpuiosaDuiahpiuhfuipasdfiupsahfpuiosaDuiahpiuhfuipasdfiupsahfpuiosaDuiahpiuhfuipasdfiupsahfpuiosaDuiahpiuhfuipasdfiupsahfpuiosaDuiahpiuhfuipasdfiupsahfpuiosaDuiahpiuhfuipasdfiupsahfpuiosaDuiahpiuhfuipasdfiupsahfpuiosa")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("El string es valido: ", value)
+	}
+
+}
+
+
+func validateStringInput(input string) (string, error) {
+
+	if len(input) == 0 {
+		return input, fmt.Errorf("El string no puede estar vacio!")
+	}
+ 
+	if len(input) > 100 {
+		return input, fmt.Errorf("El string no puede contener más de 100 caracteres!")
+	}
+
+	return input, nil
 }
