17c6653646d7adb50e8b381582bd1ad10cdd17f8
Santiago Zibecchi <santiago.zibe@gmail.com>
2023-12-27 11:05:20 -0300

    wip: concurrencia

---
+++ concurrencia/introductorio/main.go
@@ -0,0 +1,11 @@
+package main
+
+/*
+Ejercicio Introductorio
+Hacer una suma de los primeros 100 numeros, de forma secuencial
+y luego de forma concurrente y apreciar la diferencia en el tiempo de ejecución.+
+*/
+
+func main() {
+	
+}
